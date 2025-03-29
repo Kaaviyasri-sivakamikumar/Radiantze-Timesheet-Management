fc4b00f0a0cfaa43617418772c35e1272c9eaa90
Kaaviyasri-sivakamikumar <Kaaviyasri.sivakamikumar@gmail.com>
2025-03-28 21:35:36 -0700

    build fix

--- src/app/api/employee/[employeeId]/route.ts
+++ src/app/api/employee/[employeeId]/route.ts
@@ -1,6 +1,9 @@
 "use server";
-import { NextResponse, type NextRequest } from "next/server";
-import { getFirestore } from "firebase-admin/firestore";
+import { NextResponse } from "next/server";
+import { adminAuth } from "@/lib/firebase/admin";
+import { getFirestore, FieldValue } from "firebase-admin/firestore";
+import { randomBytes } from "crypto";
+const crypto = require("crypto");
 
 export type EmployeeData = {
   employeeId: string;
@@ -14,23 +17,34 @@ export type EmployeeData = {
 };
 
 export async function GET(
-  req: NextRequest,
-  { params }: { params: Record<string, string> }
+  request: Request,
+  { params }: { params: { employeeId: string } }
 ) {
-  const { employeeId } = params;
+  try {
+    const { employeeId } = params; 
 
-  const db = getFirestore();
-  const employeeRef = db.collection("employees").doc(employeeId);
-  const employeeDoc = await employeeRef.get();
+    const db = getFirestore();
+    const employeeRef = db.collection("employees").doc(employeeId);
+    const employeeDoc = await employeeRef.get();
 
-  if (!employeeDoc.exists) {
-    return NextResponse.json({ message: "Employee not found" }, { status: 404 });
+    if (!employeeDoc.exists) {
+      return NextResponse.json(
+        { message: "Employee not found" },
+        { status: 404 }
+      );
+    }
+
+    const employeeData = employeeDoc.data();
+
+    return NextResponse.json(
+      { message: "Employee details fetched successfully", response: employeeData },
+      { status: 200 } //explicit status
+    );
+  } catch (error) {
+    console.error("Error fetching employee:", error); // Log the error
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
   }
-
-  const employeeData = employeeDoc.data();
-
-  return NextResponse.json({
-    message: "Employee details fetched successfully",
-    response: employeeData,
-  });
 }
